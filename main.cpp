08930cd8d90e68dd68d1aa3de2eb7292e4523f95
立石義弘 <115062998+tateishiyoshihiro@users.noreply.github.com>
2024-02-20 16:52:26 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<iostream>
 #include<thread>
 int main() {
-	std::string a(100000, "a");
-	auto
+	std::string a(100000, 'a');
+	auto CountStart = std::chrono::system_clock::now();
+	std::string copy = a;
+	auto CountStop = std::chrono::system_clock::now();
+	std::chrono::duration<double, std::micro>Copy = CountStop - CountStart;
+	auto MoveStart = std::chrono::system_clock::now();
+	std::string move = std::move(a);
+	auto MoveStop = std::chrono::system_clock::now();
+	std::chrono::duration<double, std::micro>Move = MoveStop - MoveStart;
+	printf("100,000文字の比較\n");
+	printf("コピー");
+	std::cout << Copy.count() << "μs" << std::endl;
+	printf("移動");
+	std::cout << Move.count() << "μs" << std::endl;
+	return 0;
 }
